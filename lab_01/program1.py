93c469d888f21b658ccdbfccd4c7074210707f97
hecris <christopher.he33@myhunter.cuny.edu>
2018-09-06 10:26:46 -0400

    completed more problems and added test.py

--- lab_01/program1.py
+++ lab_01/program1.py
@@ -1,14 +1,63 @@
+# Warmup-1
+def sleep_in(weekday, vacation):
+    return (not weekday or vacation)
+
+def monkey_trouble(a_smile, b_smile):
+    return(a_smile == b_smile)
+
+def sum_trouble(a, b):
+    if a == b:
+        return (a + b) * 2
+    return a + b
+
+def diff21(n):
+    if n > 21:
+        return 2 * abs(21 - n)
+    return abs(21 - n)
+
+def parrot_trouble(talking, hour):
+    return (talking and (hour < 7 or hour > 20))
+
+def makes10(a, b):
+    return (a == 10 or b == 10 or a + b == 10)
+
+def near_hundred(n):
+    return (abs(100 - n) <= 10 or abs(200 - n) <= 10)
+
+def pos_neg(a, b, negative):
+    if negative:
+        return (a < 0 and b < 0)
+    return ((a < 0 and b > 0) or (a > 0 and b < 0))
+
+def not_string(str):
+  if len(str) >= 3 and str[:3] == "not":
+    return str
+  return "not " + str
+
+def missing_char(str, n):
+    first = str[:n]
+    back = str[n + 1:]
+    return first + back
+
+def front_back(str):
+    if len(str) <= 1:
+      return str
+    mid = str[1:-1]
+    return str[-1] + mid + str[0]
+
+def front3(str):
+    return str[:3] * 3
+
 # String-1
 
 def hello_name(name):
     return 'Hello ' + name + '!'
 
-print(hello_name('Chris'))
-
 def make_abba(a, b):
   return a + b + b + a
 
-print(make_abba('hello', 'bye'))
+def make_tags(tag, word):
+    return '<' + tag + '>' + word + '</' + tag + '>'
 
 def make_out_word(out, word):
   mid = len(out) // 2
@@ -16,15 +65,35 @@ def make_out_word(out, word):
   last_part = out[mid:]
   return first_part + word + last_part
 
-print(make_out_word('<<>>', 'chris'))
+def extra_end(str):
+    return str[-2:] * 3
+
+def first_two(str):
+    return str[:2]
+
+def first_half(str):
+    half = len(str) // 2
+    return str[:half]
+
+def without_end(str):
+    return str[1:-1]
+
+def combo_string(a, b):
+    s = a
+    l = b
+    placeholder = ''
+    if len(l) < len(s):
+        placeholder = l
+        l = s
+        s = placeholder
+    return s + l + s
 
 def non_start(a, b):
-    """Given 2 strings, return their concatenation,
-    except omit the first char of each.
-    The strings will be at least length 1."""
     return a[1:] + b[1:]
 
-print(non_start('java', 'script'))
+def left2(str):
+  return str[2:] + str[:2]
+
 
 # String-2
 def double_char(str):
@@ -32,8 +101,6 @@ def double_char(str):
   for s in str:
     result = result + s + s
   return result
-
-print(double_char('hello'))
 
 # Logic-1
 def love6(a, b):
@@ -48,9 +115,6 @@ def love6(a, b):
 def first_last6(nums):
   return (nums[0] == 6 or nums[-1] == 6)
 
-print(first_last6([6, 0, 0]))
-print(first_last6([2, 3, 3]))
-
 # List-2
 def count_evens(nums):
     count = 0
@@ -59,7 +123,13 @@ def count_evens(nums):
             count += 1
     return count
 
+def big_diff(nums):
+    minimum = nums[0]
+    maximum = nums[0]
+    for i in nums:
+        minimum = min(minimum, i)
+        maximum = max(maximum, i)
+    return maximum - minimum
 
-print(count_evens([2, 3, 4, 5, 6]))
 
     
---
+++ lab_01/tests.py
@@ -0,0 +1,12 @@
+from program1 import *
+
+def p(s):
+    print(s)
+
+    
+p(sleep_in(True, False))
+p(sum_trouble(3, 3))
+p(big_diff([99, 72, 30]))
+p(count_evens([1, 2, 3, 4, 5, 100]))
+p(double_char('hello'))
+p(first_last6([0, 2, 6]))
