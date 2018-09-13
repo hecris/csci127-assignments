d1082841c4027af3f78676e3e782492cc7e63bf3
hecris <christopher.he33@myhunter.cuny.edu>
2018-09-13 12:54:19 -0400

    add program.py

---
+++ hw_01/program.py
@@ -0,0 +1,47 @@
+def capitalize(name):
+    """
+    input: name --> a string in the form "first last"
+    output: returns a string with each of the two words capitalized
+    note: this is the one we started in class
+    """
+    return name.upper()
+
+
+def init(name):
+    """
+    Input: name --> a string in the form "first last"
+    Returns: a string in the form "F. Last" where it's a capitalized first inital 
+    and capitalized last name
+    """
+    
+    return name[0].upper() + '. ' + name.split(' ')[1]
+    #alternative
+    #return name[0].upper() + '.' + name[name.find(' '):]
+
+def part_pig_latin(name):
+    """
+    Input: A string that is a single lower case word
+    Returns: that string in fake pig latin -> move the first letter of the word to the end and add "ay"
+    so: "hello" --> "ellohay"
+    """
+    return name[1:] + name[0] + 'ay'
+
+def make_out_word(out, word):
+    mid = len(out) // 2
+    first_part = out[:mid]
+    last_part = out[mid:]
+    return first_part + word + last_part
+
+def make_tags(tag, word):
+    return '<' + tag + '>' + word + '</' + tag + '>'
+
+
+print(capitalize('Christopher He'))
+
+print(init('Christopher He'))
+
+print(part_pig_latin('hello'))
+
+print(make_out_word('<<>>', 'Hello'))
+
+print(make_tags('p', 'Paragraph.'))
