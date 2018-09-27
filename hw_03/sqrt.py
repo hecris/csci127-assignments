c1dab43ddef17b7f9cb7edc9c932e5aa409bb5b0
hecris <christopher.he33@myhunter.cuny.edu>
2018-09-27 09:46:41 -0400

    add sqrt.py

---
+++ hw_03/sqrt.py
@@ -0,0 +1,13 @@
+# Jadeja and Christopher jadeja.baptiste91@myhunter.cuny.edu
+def mysqrt(n):
+    guess = n / 2
+    old_guess = guess
+    while guess ** 2 != n:
+        guess = (guess + (n/guess)) / 2
+        if old_guess == guess:
+            return guess
+        old_guess = guess
+    return guess
+
+for i in range(1,101):
+    print(i, mysqrt(i))
