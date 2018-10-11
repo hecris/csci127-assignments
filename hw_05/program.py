ae8dcf35cffbd333433b3bf935b78e143817832c
hecris <christopher.he33@myhunter.cuny.edu>
2018-10-11 12:14:28 -0400

    complete hw_05

---
+++ hw_05/program.py
@@ -0,0 +1,17 @@
+def filterodd(l):
+    for i in l:
+        if i % 2 == 0:
+            l.remove(i)
+    return l
+
+def mapsquare(l):
+    res = []
+    for i in l:
+        res.append(i**2)
+    return res
+
+if __name__ == '__main__':
+    l = [1, 2, 3, 4, 5]
+    print('original list:', l)
+    print(filterodd(l))
+    print(mapsquare(l))
