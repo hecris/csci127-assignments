63f92f0162f0f18cb85cf88abeebeda14d1db4fd
hecris <christopher.he33@myhunter.cuny.edu>
2018-11-12 10:56:33 -0500

    update hw_08

--- hw_08/words.py
+++ hw_08/words.py
@@ -11,16 +11,14 @@ def clean(text):
 def analyze(text):
     text = clean(text)
     l = text.split()
-    d = {} # make an empty dictionary
-    for i in l:
-        d.setdefault(i, []) # fill dictionary with unique words from the text
+    d = {}
 
-    i = 0
-    while i < len(l) - 1:
+    for i in range(len(l)-1):
         w = l[i]
-        d[w] += [l[i + 1]] # add next word to the dictionary
-        i += 1
-    
+        d.setdefault(w, [])
+        n = l[i + 1] # next word
+        d[w].append(n)
+
     return d
 
 f = open('moby-small.txt', 'r')
